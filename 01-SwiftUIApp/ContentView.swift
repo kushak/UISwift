3039a938b31808164d840c7cc31a7439b883e02c
oShipulin <shipulin@restream.rt.ru>
2019-11-29 14:56:13 +0300

    Initial Commit

---
+++ 01-SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01-SwiftUIApp
+//
+//  Created by Oleg Shipulin on 29.11.2019.
+//  Copyright © 2019 Oleg Shipulin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
